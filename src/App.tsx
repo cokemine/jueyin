f97403389db71639a1cf92b3432d3e06eb93aa30
cokemine <aptx4561@gmail.com>
2022-01-10 22:20:50 +0800

    feat(components): add global header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react';
+import Header from './components/Header';
+import './App.scss';
 
 const App: FC = () => (
-  <div>Hello, React</div>
+  <>
+    <Header />
+    <div>Hello, React</div>
+  </>
 );
 
 export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react';
+import './style.scss';
+import logo from '../../assets/logo.svg';
+import logoMobile from '../../assets/logo-mobile.svg';
+
+const Header: FC = () => (
+  <div className="main-header">
+    <div>
+      <a href="/">
+        <img src={logo} alt="" className="logo" />
+        <img src={logoMobile} className="logo-mobile" />
+      </a>
+    </div>
+    <div className="nav-list">
+      <ul className="list list-left">
+        {
+          ['首页', '沸点', '课程', '资讯', '活动', '开放社区'].map(item => (
+            <li className="list-left__item">
+              <a href="#">{item}</a>
+            </li>
+          ))
+        }
+      </ul>
+      <ul className="list list-right">
+        <input type="text" className="search" placeholder="搜索" />
+        <button className="button button__add" type="button">创作者中心</button>
+        <button className="button button__login" type="button">登录</button>
+      </ul>
+    </div>
+  </div>
+);
+
+export default Header;
