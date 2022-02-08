4d20da36b46e291629f6460b058f2a6490b82257
cokemine <aptx4561@gmail.com>
2022-02-08 23:37:21 +0800

    fix: fix unique id

--- src/api/wrapper.ts
+++ src/api/wrapper.ts
@@ -2,9 +2,28 @@ import {
   getCategories, getArticles, getArticleById, getCommentsByArticleId
 } from './index';
 
+/* 提供的数据有 ID 重复 */
+async function getArticlesWrapper(...args: any[]) {
+  // eslint-disable-next-line prefer-spread
+  const { data: articles } = await getArticles.apply(null, args as any);
+  const patch = new Set();
+  return {
+    code: 0,
+    data: {
+      articles: articles.articles.filter(article => {
+        if (patch.has(article.article_id)) {
+          return false;
+        }
+        patch.add(article.article_id);
+        return true;
+      })
+    }
+  };
+}
+
 const Api = {
   getCategories,
-  getArticles,
+  getArticles: getArticlesWrapper,
   getArticleById,
   getCommentsByArticleId
 } as Record<string, any>;
