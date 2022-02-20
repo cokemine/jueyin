2a5584279c9ffe547e3ed2342b726d60bc96cc2d
cokemine <aptx4561@gmail.com>
2022-02-20 10:09:32 +0800

    fix type

--- src/api/index.d.ts
+++ src/api/index.d.ts
@@ -1,4 +1,6 @@
-import { ICategories, Response, IArticles } from '../types';
+import {
+  ICategories, Response, IArticles, IArticle
+} from '../types';
 
 export declare function getCategories(): Promise<Response<ICategories>>;
 
@@ -6,6 +8,6 @@ export declare function getArticles(
   categoryId: number, sortBy: 'hot' | 'new', offset: number, limit: number
 ): Promise<Response<IArticles>>;
 
-export declare function getArticleById(articleId: number): Promise<Response>;
+export declare function getArticleById(articleId: number): Promise<Response<IArticle>>;
 
 export declare function getCommentsByArticleId(articleId: number, offset: number, limit: number): Promise<Response>;
