63a8b9324e54d7fb8db65622c91aaa2152ff11da
cokemine <aptx4561@gmail.com>
2022-02-24 23:28:38 +0800

    fix: fix mocked articles api response missing info

--- src/api/wrapper.ts
+++ src/api/wrapper.ts
@@ -5,12 +5,12 @@ import {
 /* 提供的数据有 ID 重复 */
 async function getArticlesWrapper(...args: any[]) {
   // eslint-disable-next-line prefer-spread
-  const { data: articles } = await getArticles.apply(null, args as any);
+  const res = await getArticles.apply(null, args as any);
   const patch = new Set();
   return {
-    code: 0,
+    ...res,
     data: {
-      articles: articles.articles.map(article => {
+      articles: res.data.articles.map(article => {
         if (patch.has(article.article_id)) {
           article.article_id = Math.random().toString();
         }
