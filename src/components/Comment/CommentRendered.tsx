cokemine/jueyin

commit a9d8fbbad847e787f4f5f99ac447850036701ed0

refactor: refactor with `forwardRef`

src/components/Comment/CommentRendered.tsx
@@ -33,7 +33,7 @@ const CommentRendered: FC<Props> = ({
             createAt={comment.comment_info.ctime}
             likeCount={comment.comment_info.digg_count}
             data-comment-index={offset + index}
-            refCallback={observeCallback && ((el: Element) => (el && observeCallback(el)))}
+            ref={((el: HTMLDivElement) => observeCallback && observeCallback(el))}
           />
         ))
       }

src/components/Comment/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { forwardRef } from 'react';
 import { AiOutlineComment, AiOutlineLike } from 'react-icons/ai';
 import classNames from 'classnames';
 import defaultAvatar from '../../assets/avatar.jpg';
@@ -26,10 +26,9 @@ type Props = {
   likeCount: number;
   /* father comment index for observer used */
   'data-comment-index'?: number | undefined;
-  refCallback?: React.RefCallback<Element> | undefined;
 };
 
-const Comment: FC<Props> = props => {
+const Comment = forwardRef<HTMLDivElement, Props>((props, ref) => {
   const wrapperClass = classNames('comment-item', {
     'comment-item--sub': props.isSub
   });
@@ -47,7 +46,7 @@ const Comment: FC<Props> = props => {
   }
 
   return (
-    <div className={wrapperClass} data-comment-index={props['data-comment-index']} ref={props.refCallback}>
+    <div className={wrapperClass} data-comment-index={props['data-comment-index']} ref={ref}>
       <Image className={avatarClass} defaultSrc={defaultAvatar} src={props.avatarUrl} alt={props.name} />
       <div className="comment-item__main">
         <div className="comment-item__top">
@@ -102,7 +101,7 @@ const Comment: FC<Props> = props => {
       </div>
     </div>
   );
-};
+});
 
 Comment.defaultProps = {
   isSub: false,
@@ -110,8 +109,7 @@ Comment.defaultProps = {
   replyToName: null,
   replyToContent: null,
   authorDesc: '',
-  'data-comment-index': undefined,
-  refCallback: undefined
+  'data-comment-index': undefined
 };
 
 export default Comment;

commit c62f13d4e1d31d65af7d1e509160d2d0e58b303e

fix: fix observer error
seems like https://github.com/cokemine/jueyin/commit/a9d8fbbad847e787f4f5f99ac447850036701ed0 caused this

src/components/Comment/CommentRendered.tsx
@@ -33,7 +33,7 @@ const CommentRendered: FC<Props> = ({
             createAt={comment.comment_info.ctime}
             likeCount={comment.comment_info.digg_count}
             data-comment-index={offset + index}
-            ref={((el: HTMLDivElement) => observeCallback && observeCallback(el))}
+            ref={observeCallback && ((el: HTMLDivElement) => el && observeCallback(el))}
           />
         ))
       }